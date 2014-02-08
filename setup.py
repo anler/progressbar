dadd049c76116700cbb866d1e274f68feffca2fe
Mark Mossberg <mark.mossberg@gmail.com>
2014-02-08 05:04:35 -0500

    add setup.py to enable pip install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='progressbar',
+        version='0.1',
+        description='Utility module to represent progress in the form of a progress bar.',
+        url='http://github.com/ikame/progressbar',
+        author='Anler Peral',
+        author_email='anler86@gmail.com',
+        license='MIT',
+        packages=find_packages()
+)
